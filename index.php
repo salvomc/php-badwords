7456a1d52483cbd9eeb53aae4643a87f3cd095d8
salvomc <smcrwrk@gmail.com>
2023-07-03 16:42:55 +0200

    censorship function done

--- index.php
+++ index.php
@@ -6,6 +6,13 @@
         <title>php-badwords</title>
     </head>
     <body>
-        
+         <div class>
+            <h1>php-badwords</h1>
+            <form action="server.php" method="GET">
+                <textarea name="paragraph" placeholder="Insert Text"></textarea>
+                <input type="text" name="word-to-censor" placeholder="Insert word to censor">
+                <button type="submit">Send</button>
+            </form>
+        </div>
     </body>
 </html>
--- server.php
+++ server.php
@@ -1,2 +1,13 @@
 <?php
+
+$wordToCensor = $_GET['word-to-censor'];
+
+$paragraph = $_GET['paragraph'];
+
+echo "Paragraph / Paragraph Lenght"."<br>".$paragraph." / ".strlen($paragraph)."<br><br>";
+
+$censoredParagraph = str_replace($wordToCensor, '***', $paragraph);
+
+echo "Censored Paragraph / Censored Paragraph Lenght"."<br>".$censoredParagraph." / ".strlen($censoredParagraph);
+
 ?>
